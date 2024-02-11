f01b1fbb6d348d42067e27c0fefb75f0f5f9d77c
eminentishere <eminentishere@outlook.com>
2024-02-12 02:55:05 +0500

    day 1 completed

---
+++ Projects/Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the city of Shaheer's Aidah❣️")
+city = input("What's your city you grew up in? \n")
+pet_name = input("What's your favorite pet name? \n")
+print("Your band name could be " + city + " " + pet_name + " 🫡🫡")
